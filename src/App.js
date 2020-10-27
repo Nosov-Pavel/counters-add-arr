d6594f6f6769bfacfd1003cb410e94309ff3cf88
Nosov-Pavel <nopaev@gmail.com>
2020-10-26 22:07:41 -0400

    &&

--- src/App.js
+++ src/App.js
@@ -13,28 +13,28 @@ function App() {
         const newList = [...list, {id:Math.random(), counter: 0}];
         setList(newList);
     }
-    const moveCounterUp = (index) => {
-        const newList = [...list];
-
-        const currentEl = newList[index];
-        newList[index] = newList[index - 1];
-        newList[index-1] = currentEl;
-
-        setList(newList)
-
-
-    }
-    const moveCounterDown = (index) => {
-        const newList = [...list];
-
-        const currentEl = newList[index];
-        newList[index] = newList[index + 1];
-        newList[index + 1] = currentEl;
-
-        setList(newList)
-
-
-    }
+    // const moveCounterUp = (index) => {
+    //     const newList = [...list];
+    //
+    //     const currentEl = newList[index];
+    //     newList[index] = newList[index - 1];
+    //     newList[index-1] = currentEl;
+    //
+    //     setList(newList)
+    //
+    //
+    // }
+    // const moveCounterDown = (index) => {
+    //     const newList = [...list];
+    //
+    //     const currentEl = newList[index];
+    //     newList[index] = newList[index + 1];
+    //     newList[index + 1] = currentEl;
+    //
+    //     setList(newList)
+    //
+    //
+    // }
     const moveCounter = (currentIndex, nextIndex) => {
         const newList = [...list];
 
@@ -56,7 +56,7 @@ function App() {
                 return(
                     <div>
                         {el.counter}
-                        <button onClick={() => moveCounter (i, i-1)}>⇡</button>
+                        <button onClick={() => moveCounter (i, i -1)}>⇡</button>
                         <button onClick={() => moveCounter(i, i+1)}>⇣</button>
                     </div>
 
