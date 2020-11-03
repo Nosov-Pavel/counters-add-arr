69209330c19225a5796441f9ecfb4a3b457c6216
Nosov-Pavel <nopaev@gmail.com>
2020-11-03 13:30:32 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 
 function App() {
+    const initState = [
+        {id:1, title:'Fist', description: 'learn HTML'},
+        {id:2, title:'Second', description: 'learn CSS'},
+        {id:3, title:'Third', description: 'learn JS'},
+        {id:4, title:'Forth', description: 'learn REACT'},
+    ]
+    return(
+        <div>
+
+
+        </div>
+    )
 
 }
 export default App;
