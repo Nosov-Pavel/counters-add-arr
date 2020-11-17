d13251e593d5c0b7f3bbd881713b55e09e36dd2f
Nosov-Pavel <nopaev@gmail.com>
2020-11-17 18:14:55 -0500

    isAddMode

--- src/ToDoController.js
+++ src/ToDoController.js
@@ -3,35 +3,41 @@ import './App.css';
 
 
 function TodoController(props) {
-const [newTitle, setNewTitle] = useState('');
-const [newDescription, setNewDescription] = useState('');
+    const [newTitle, setNewTitle] = useState('');
+    const [newDescription, setNewDescription] = useState('');
+    const [isAddModeOn, setIsAddModeOn] = useState(false);
 
-const addNewButtonHandler = () => {
-    props.addTodo(newTitle, newDescription);
-    setNewTitle('');
-    setNewDescription('');
-}
-
+    const addNewButtonHandler = () => {
+        props.addTodo(newTitle, newDescription);
+        setNewTitle('');
+        setNewDescription('');
+    }
 
 
     return (
         <div>
             <button>add new todo</button>
-            <label>title:</label>
-            <input type="text"
-                   value={newTitle}
-                   onChange={(event) => setNewTitle(event.target.value)}/>
+            {false &&
+                <>
+                    <label>title:</label>
+                    <input type="text"
+                           value={newTitle}
+                           onChange={(event) => setNewTitle(event.target.value)}/>
 
-            <label>description:</label>
-            <input type="text"
-                   value={newDescription}
-                   onChange={(event) => setNewDescription(event.target.value)}/>
+                    <label>description:</label>
+                    <input type="text"
+                           value={newDescription}
+                           onChange={(event) => setNewDescription(event.target.value)}/>
 
-            <button onClick={addNewButtonHandler}>add new todo</button>
+                    <button onClick={addNewButtonHandler}>save</button>
+                </>
+            }
+
             <hr/>
 
         </div>
     );
 
 }
+
 export default TodoController;
