d3e04741c8d01322f6cd3531da25f6dff5f0acbb
Nosov-Pavel <nopaev@gmail.com>
2020-11-18 13:23:41 -0500

    titleLabel

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function ToDo(props) {
+    const [newTitle, setNewTitle] = useState('');
+
 
 
     return (
@@ -12,6 +14,8 @@ function ToDo(props) {
             <button onClick={() => props.deleteTodo(props.todo.id)}>delete</button>
             <button disabled={props.index === 0} onClick={() => props.moveTodo(props.index, props.index -1)}>⇡</button>
             <button disabled={props.isLast} onClick={() => props.moveTodo(props.index, props.index +1)}>⇣</button>
+            <label>title: </label>
+            <input type="text"/>
             <button>update</button>
             <br/>
         </div>
